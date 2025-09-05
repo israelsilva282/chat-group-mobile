9fcb3d0393913c587bbb27bc2505dcc95df924dc
israelsilva282 <israelcostas@hotmail.com>
2025-09-05 15:10:53 -0300

    Adicionado login e criar conta

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,56 @@
+import { useNavigation } from '@react-navigation/native';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { useState } from 'react';
 import { Platform, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
+import { auth } from '../../services/firebaseConfig';
 
 export default function SignIn() {
+    const navigation = useNavigation();
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const [type, setType] = useState(false); // false = login, true = cadastro
+
+    function handleLogin() {
+        if (type) {
+            //Cadastro
+            if (name === '' || email === '' || password === '') {
+                alert('Preencha todos os campos!');
+                return;
+            }
+
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((user) => {
+                    console.log(user)
+                    user.user.updateProfile({ displayName: name })
+                        .then(() => { navigation.goBack(); });
+                })
+                .catch((error) => {
+                    console.log(error)
+                    if (error.code === 'auth/email-already-in-use') {
+                        alert('Esse email já está em uso!');
+                        return;
+                    }
+                    if (error.code === 'auth/invalid-email') {
+                        alert('Email inválido!');
+                        return;
+                    }
+                });
+        } else {
+            //login
+            signInWithEmailAndPassword(auth, email, password)
+                .then(() => {
+                    navigation.goBack();
+                })
+                .catch((error) => {
+                    if (error.code === 'auth/invalid-email') {
+                        alert('Email inválido!');
+                        return;
+                    }
+                })
+        }
+    }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -36,16 +80,20 @@ export default function SignIn() {
                 onChangeText={setPassword}
                 placeholder='Sua senha'
                 placeholderTextColor={'#99999b'}
+                secureTextEntry={true}
             />
 
-            <TouchableOpacity style={[styles.buttonLogin, { backgroundColor: type ? '#f53745' : '#57db86' }]}>
+            <TouchableOpacity
+                style={[styles.buttonLogin, { backgroundColor: type ? '#f53745' : '#57db86' }]}
+                onPress={handleLogin}
+            >
                 <Text style={styles.buttonText}>{type ? 'Cadastrar' : 'Acessar'}</Text>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => setType(!type)}>
                 <Text>{type ? 'Já possuo uma conta' : 'Criar uma nova conta'}</Text>
             </TouchableOpacity>
-        </SafeAreaView>
+        </SafeAreaView >
     );
 }
 
